1665a7f8fd05d70ee47b3c9604e2768fe6721f10
Bajtix <39443948+Bajtix@users.noreply.github.com>
2020-10-03 19:41:22 +0200

    levels

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -6,13 +6,25 @@ public class Door : MonoBehaviour
 {
     public Animator animator;
 
+
+    public float autocloseTime = -1;
     public void Open()
     {
         animator.SetBool("open",true);
+
+        if (autocloseTime > 0)
+            StartCoroutine("CloseTask");
+
     }
 
     public void Close()
     {
         animator.SetBool("open", false);
+    }
+
+    private IEnumerator CloseTask()
+    {
+        yield return new WaitForSeconds(autocloseTime);
+        Close();
     }
 }
